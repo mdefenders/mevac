1c9cb52e38fe622144f68fc200bc093a9582e330
den <aditor@gmail.com>
2024-03-24 20:59:07 +0200

    tag fix v2

--- mevaclibs/importers.py
+++ mevaclibs/importers.py
@@ -175,6 +175,8 @@ class MstImporter:
                 sensitive = 0
             language, text = list(post['object']['contentMap'].items())[0]
             text = BeautifulSoup(text, 'html.parser').get_text(separator='\n')
+            text = text.replace('#\n', '#')
+            text = text.replace('@\n', '@')
             if text != '' or post['object']['attachment']:
                 posts_count += 1
                 try:
